9bd583d472d7e134a695ec4dab48b49f09098eaa
itsbimaps14 <61398214+itsbimaps14@users.noreply.github.com>
2022-04-16 14:04:22 +0700

    Update Fix Bug + Ready Create TC

---
+++ src/Main/Java/App.java
@@ -0,0 +1,14 @@
+package Main.Java;
+
+import Main.Java.NumberDifferenceFlow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+    	NumberDifferenceFlow application = new NumberDifferenceFlow();
+    	application.execute(); // call application process
+    }
+}
---
+++ src/Test/Java/NumberDifferenceTest.java
@@ -0,0 +1,62 @@
+package Test.Java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Main.Java.*;
+
+public class NumberDifferenceTest {
+	private CalcNUmberDifference sut;
+	
+	/**
+	 * Setup = inisiasi sebelum test dijalankan, preparation object
+	 * --> pay attention to keep components and test cases small, 
+	 * --> the trade-off seems acceptable. 
+	 * --> implicit setup can be used to deine the fixture common for all tests. 
+	 * --> Small in-line and delegate setup statements may supplement .. 
+	 *     the speciic preconditions on per-test basis. 
+	 * */
+	@Before
+	public void setUp() throws Exception {
+		sut = new CalcNUmberDifference();
+	}
+        
+        /* TC 1 - 5 */
+        
+	@Test
+	public void testInputNBilLuarRange_01() {
+		// (1) setup (arrange, build)
+		int jumlahDeretBil;
+		boolean actual, expected;
+		jumlahDeretBil = 15; // bukan berada pada range 1-10
+		expected = false;
+		
+		// (2) exercise (act, operate)
+		actual = sut.validateRangeInputMaxDeret(jumlahDeretBil);
+		
+		// (3) verify (assert, check)
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testInputNBilDalamRange_01() {
+		// (1) setup (arrange, build)
+		int jumlahDeretBil;
+		boolean actual, expected;
+		jumlahDeretBil = 5; // berada pada range 1-10
+		expected = true;
+		
+		// (2) exercise (act, operate)
+		actual = sut.validateRangeInputMaxDeret(jumlahDeretBil);
+		
+		// (3) verify (assert, check)
+		assertEquals(expected, actual);
+	}
+        
+        /* TC 6 - 10 */
+        
+        
+        /* TC 11 - 15 */
+}
